1e35be5336923e828c1231cdd724acb830bd3315
Jean <jeanpolo15@gmail.com>
2023-09-12 18:10:49 -0500

    redes

--- src/componentes/Footer/index.jsx
+++ src/componentes/Footer/index.jsx
@@ -3,13 +3,13 @@ import "./Footer.css"
 const Footer = () => {
     return <footer className='footer' style={{ backgroundImage: "url(/img/footer.png)" }}>
         <div className='redes'>
-            <a href='https://www.aluracursos.com/'>
+            <a href='hhttps://www.facebook.com/jean.polo.3762/'>
                 <img src="/img/facebook.png" alt='Facebook' />
             </a>
-            <a href='https://www.aluracursos.com/'>
-                <img src="/img/twitter.png" alt='twitter' />
+            <a href='https://www.linkedin.com/in/jean-marcos-polo-parker-087951271/'>
+                <img src="/img/linkedin.svg" alt='LinkedIn' className="linkedin" />
             </a>
-            <a href='https://www.aluracursos.com/'>
+            <a href='https://www.instagram.com/jeanmarcos_pp09/'>
                 <img src="/img/instagram.png" alt='instagram' />
             </a>
         </div>
